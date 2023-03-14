bb5ac4cec9215b241b8f8707bb45b81e2bc363f0
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-03-15 00:50:12 +0300

    pdf module is ready

--- DocPet/pdf2xls.py
+++ DocPet/pdf2xls.py
@@ -1,7 +1,17 @@
 
-""" from https://github.com/danshorstein/pythonic-accountant/blob/master/019%20Importing%20data%20from%20another%20PDF%20file/pdf_extract.ipynb"""
+"""
+модуль подготовки данных для формирования финансового отчета
+from https://github.com/danshorstein/pythonic-accountant/blob/master/019%20Importing%20data%20from%20another%20PDF%20file/pdf_extract.ipynb
+
+
+"""
+
 import pdfplumber
 import re
+import os
+import json
+
+DATA_DIR = 'data'
 
 point_re = re.compile(r'(\d{4}\s)')
 acc_point_re = r'(\d{4}\s)'
@@ -21,13 +31,22 @@ def tailConverter(result):
         return [None]
     return tail
 
+def PdfFilesList(pdf_dir = DATA_DIR):
+    """ возвращает список файлов в директории """
+    result_dict = {pdf_dir: {}}
+    for file in os.listdir(f"{pdf_dir}/"):
+        name = file.split('.')[0]
+        extention = file.split('.')[1]
+        if extention == 'pdf':
+            result_dict[DATA_DIR][name] = file
+    return  result_dict
 
 
-def GetInfoFromPDF():
+
+def GetInfoFromPDF(pdf_file="data/2020.pdf"):
     """ берет информацию со всех файлов в директории data и возвращает словарь с указанием года и значениями позиций"""
     document = dict()
-    with pdfplumber.open("data/2020.pdf") as pdf:
-        pages = pdf.pages
+    with pdfplumber.open(pdf_file) as pdf:
         for page_number, page in enumerate(pdf.pages):
             page_data = dict()
             text = page.extract_text()
@@ -40,16 +59,28 @@ def GetInfoFromPDF():
                     result = line[position:]
                     comp_exclude = point_re_exclude.search(result)
                     if comp_exclude:
-                        print(f'Excluded {line=}\n {result=}')
+                        continue
+                        # print(f'Excluded {line=}\n {result=}')
                     else:
                         values = tailConverter(result)
                         if 1 < len(values) < 5:
                             document[comp.group()[:-1]] = values
-                            print(f'{position=} {comp.group()=}: {values=}\n {line} tail {result=}')
+                            # print(f'{position=} {comp.group()=}: {values=}\n {line} tail {result=}')
+
+    return document
+
+def GetInfoFromAllPDF():
+    """ возвращает словарь с данными из указанных файлов """
+    list_files = PdfFilesList(pdf_dir=DATA_DIR) # запрос на список файлов в директории
+    data_dict = dict()
+    for name, file in list_files[DATA_DIR].items():
+        data_dict[name] = GetInfoFromPDF(pdf_file=f"{DATA_DIR}/{file}")
+    with open(f'{DATA_DIR}/data.json', 'w') as fp:
+        json.dump(data_dict, fp)
+    return data_dict
 
 
-
-if '__name__' == '__main__':
-    pass
+if __name__ == '__main__':
+    print(GetInfoFromAllPDF())
 
 
